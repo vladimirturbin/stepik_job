d3a3c0788a10198e0cbeadcc2798df04e34b1c24
Vladimir Turbin <vladimirturbin@gmail.com>
2020-05-17 21:32:04 +0300

    cosmetic changes

--- job/management/commands/import.py
+++ job/management/commands/import.py
@@ -8,42 +8,41 @@
 #     help = 'Import data from data.py'
 #
 #     def handle(self, *args, **options):
+#         for i in Vacancy.objects.all():
+#             i.skills = 'IDE • debug • GIT • coding • docker'
+#             i.save()
 #
-#         # for i in companies:
-#         #     Company.objects.create(name=i['title'], employee_count=0)
+# #         # for i in companies:
+# #         #     Company.objects.create(name=i['title'], employee_count=0)
+# #
+# #         # for i in Company.objects.all():
+# #         #     print(i.id, i.name,i.logo)
+# #         #     i.logo = f'job/logo{i.id}.png'
+# #         #     i.save()
+# #
+# #
+# #         # for i in specialties:
+# #         #     Speciality.objects.create(code=i['code'], title=i['title'])
+# #
+# #         # for i in Speciality.objects.all():
+# #         #
+# #         #     i.picture = f'job/specty_{i.code}.png'
+# #         #     i.save()
+# #         #     print(i.id, i.code, i.title, i.picture)
+# #
+# #
+# #         # for i in jobs:
+# #         #
+# #         #     Vacancy.objects.create(
+# #         #         title=i['title'],
+# #         #         specialty=Speciality.objects.get(code=i['cat']),
+# #         #         company=Company.objects.get(name=i['company']),
+# #         #         text=i['desc'],
+# #         #         salary_min=int(i['salary_from']),
+# #         #         salary_max=int(i['salary_to']),
+# #         #         published_at=datetime.strptime(i['posted'],
+# #         '%Y-%m-%d').date()
+# #         #     )
+# #         # Vacancy.objects.all().update(skills='123')
+# #
 #
-#         # for i in Company.objects.all():
-#         #     print(i.id, i.name,i.logo)
-#         #     i.logo = f'job/logo{i.id}.png'
-#         #     i.save()
-#
-#
-#         # for i in specialties:
-#         #     Speciality.objects.create(code=i['code'], title=i['title'])
-#
-#         # for i in Speciality.objects.all():
-#         #
-#         #     i.picture = f'job/specty_{i.code}.png'
-#         #     i.save()
-#         #     print(i.id, i.code, i.title, i.picture)
-#
-#
-#         # for i in jobs:
-#         #
-#         #     Vacancy.objects.create(
-#         #         title=i['title'],
-#         #         specialty=Speciality.objects.get(code=i['cat']),
-#         #         company=Company.objects.get(name=i['company']),
-#         #         text=i['desc'],
-#         #         salary_min=int(i['salary_from']),
-#         #         salary_max=int(i['salary_to']),
-#         #         published_at=datetime.strptime(i['posted'],
-#         '%Y-%m-%d').date()
-#         #     )
-#         # Vacancy.objects.all().update(skills='123')
-#
-#         # for i in Vacancy.objects.all():
-#         #
-#         #     i.skills = 'Lorem•ipsum•dolor•sit•amet'
-#         #     i.save()
-#         pass
