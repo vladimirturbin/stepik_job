dc469c2240b6d592d2309664d4ac02228c1619e9
Vladimir Turbin <vladimirturbin@gmail.com>
2020-05-28 23:13:09 +0300

    forms experiments

--- job/forms.py
+++ job/forms.py
@@ -1,6 +1,38 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit, Reset
+from crispy_forms.bootstrap import PrependedText, AppendedText, FormActions
 from django import forms
 
 
 class RegisterForm(forms.Form):
     email = forms.EmailField()
-    password = forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.form_method = 'post'
+
+        self.helper.form_class = 'form-control'
+        self.helper.label_class = 'text-muted'
+        self.helper.field_class = 'col-lg-12'
+
+        self.helper.layout = Layout(
+            # Fieldset(
+            #     'ADDRESSES',
+            #     AppendedText('address', '&#9993;',
+            #                  placeholder="почтовый адрес"),
+            #     PrependedText('email', '@', placeholder="email"),
+            #     AppendedText('date_of_delivery', '',
+            #                  placeholder='дата доставки'),
+            # ),
+            Fieldset(
+                'MAIN',
+                'email',
+                'password',
+            ),
+            FormActions(
+                Submit('submit', 'Submit'),
+            )
+        )
